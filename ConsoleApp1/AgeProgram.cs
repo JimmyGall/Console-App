90c2b8a382e8a7bc2640535e8d17c511c950c9b5
Jimmy Gall <GAL202201255@longroad.ac.uk>
2022-09-27 09:51:14 +0100

    Added the age check program

---
+++ ConsoleApp1/AgeProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AgeProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your age?");
+
+            if (Convert.ToInt64(Console.ReadLine()) >= 17)
+            {
+                Console.WriteLine("You are old enough to learn to drive.");
+            }
+            else
+            {
+                Console.WriteLine("You are not old enough to learn to drive.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

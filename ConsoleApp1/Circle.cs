fb16f264aea6b3ff338ef7d47bad202fc6d28df2
Jimmy Gall <GAL202201255@longroad.ac.uk>
2022-10-04 10:33:36 +0100

    Added Circle problem

---
+++ ConsoleApp1/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Circle
+    {
+
+        public static void properties(double diameter, double angle)
+        {
+            double radius = diameter / 2;
+            double area = Math.PI * Math.Pow(radius, 2);
+            double circumference = Math.PI * diameter;
+            double arc_length = (circumference * angle) / 360;
+
+            Console.WriteLine("Radius: " + radius);
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Circumference: " + circumference);
+            Console.WriteLine("Arc length: " + arc_length);
+
+            Console.ReadKey();
+        }
+
+    }
+}

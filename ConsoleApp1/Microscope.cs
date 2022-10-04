b291ff726fa28cdab4c928d6d6907cbc0cd861fb
Jimmy Gall <GAL202201255@longroad.ac.uk>
2022-10-04 10:52:23 +0100

    Added microscope problem

---
+++ ConsoleApp1/Microscope.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Microscope
+    {
+        public static void Calculate(double sizeInCM, double micrometers)
+        {
+            double size = micrometers / sizeInCM;
+
+            Console.WriteLine("Size: " + size);
+            Console.ReadKey();
+        }
+    }
+}

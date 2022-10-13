624d465abdfed4885a7d1ff715ba44ed5c76e686
Jimmy Gall <GAL202201255@longroad.ac.uk>
2022-10-13 12:13:55 +0100

    Added boolean logic

---
+++ ConsoleApp1/BooleanLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BooleanLogic
+    {
+
+        public void logic()
+        {
+            int x;
+            x = 2 + 7;
+
+            Console.WriteLine("Value = " + x);
+
+            if (x == 6)
+            {
+                Console.WriteLine("True");
+            } else
+            {
+                Console.WriteLine("false");
+            }
+        }
+
+    }
+}

c58f1c787af96c4cb5d4ec1c08e187f85b9323c7
Jimmy Gall <GAL202201255@longroad.ac.uk>
2022-11-03 12:17:03 +0000

    AMOGUS??

---
+++ ConsoleApp1/CarValue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class CarValue
+    {
+
+        public static void Value(int year, double carValue, double minResaleValue)
+        {
+            if (year > 1) ;
+            year--;
+
+            carValue *= 0.70;
+
+            if (carValue > minResaleValue) ;
+
+            for (int i = year; i < year; i++)
+            {
+                if (carValue > minResaleValue) ;
+                carValue *= 0.80;
+                Console.WriteLine("Year " + year + ", Value: " + carValue);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            CompundInterest.interest(100, 4);
+            CarValue.Value(2, 12000, 4000);
             //
         }
     }
